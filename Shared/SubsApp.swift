ccf56e3ef39a9aca90b35d6fb7da5c55dbdb4566
Mahmoud Eldesouky <eldesouky@dolph.in>
2021-05-23 13:48:24 +0200

    Initial Commit

---
+++ Shared/SubsApp.swift
@@ -0,0 +1,20 @@
+//
+//  SubsApp.swift
+//  Shared
+//
+//  Created by Mahmoud Eldesouky on 23.05.21.
+//
+
+import SwiftUI
+
+@main
+struct SubsApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
